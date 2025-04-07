611ae00d18d99eb6dff325062c06aece0cc70c12
Ruslan77code <59168327+Ruslan77code@users.noreply.github.com>
2025-04-07 17:51:38 +0300

    Final Project

---
+++ index.js
@@ -0,0 +1,24 @@
+const imageContainerEl = document.querySelector('.image-container')
+
+const btnEl = document.querySelector('.btn')
+
+btnEl.addEventListener('click', ()
+=> {
+  let imageNum = 10;
+  addNewImages();
+});
+
+function addNewImages(){
+for(let index = 0; index<array.
+  imageNum;index++){
+    const element = array[index];
+  }
+
+  const newImgEl = document.
+  createElement("img");
+newImgEl.src=`https://picsum.
+photos/300?random=${Math.floor(
+  Math.random()*2000
+)}`;
+imageContainerEl.appendChild(newImgEl)
+}
